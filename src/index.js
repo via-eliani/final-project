9461dd6c0a1a41b589c2eb1f7ba3b32f7810e6a1
Eliani <via.eliani@gmail.com>
2020-06-28 22:11:47 -0400

    Added functions that connect to forecast api

--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
 //***START: defined functions***
 
-//called from "main" function or changeDisplay function
-//defines day, hour, minutes, and time
-//changes HTML to display day and time
+/*displayDate function
+-- called from "main" function or changeDisplay function
+-- defines day, hour, minutes, and time
+-- changes HTML to display day and time*/
 function displayDate() {
   let now = new Date();
   //array of days of the week
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  //array of months of the year
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
   //local variables
   let day = days[now.getDay()];
   let hour = now.getHours();
@@ -35,31 +21,37 @@ function displayDate() {
   time.innerHTML = `${hour}:${minutes}`; //changes display of time
   today.innerHTML = `${day}`; //changes display of day
 }
-//obtains user input
-//calls searchCity within addEventListener
+/*submitCity function
+-- obtains user input
+-- calls searchCity within addEventListener*/
 function submitCity() {
   //query Selectors
   let search = document.querySelector("#search-city");
 
   search.addEventListener("submit", searchCity);
 }
-//called from addEventListener
-//obtains user input
-//connects to Weather API
-//calls changeDisplay function
+/*searchCity function
+-- called from addEventListener
+-- obtains user input
+-- connects to Weather API
+-- calls changeDisplay function
+-- calls retruieveHourly function*/
 function searchCity(event) {
   event.preventDefault();
   //query Selectors
   let inputCity = document.querySelector("#input-city");
   //local variables
+  let theCity = inputCity.value;
   let apiKey = `f655251e7aa74c3031f8eb126912bec6`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&appid=${apiKey}&units=metric`;
-  //links to weatherAPI
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${theCity}&appid=${apiKey}&units=metric`;
+  //connects to weatherAPI
   axios.get(apiUrl).then(changeDisplay);
 }
-//called from searchCity function or getTemp function
-//obtains current temperature in C degrees
-//calls displayDate function
+/*changeDisplay function
+-- called from searchCity function or getTemp function
+-- obtains current temperature in C degrees
+-- calls displayDate function
+-- calls retrieveHourly function*/
 function changeDisplay(response) {
   fahrenheitLink.classList.remove("active"); //removes active class in HTML
   celsiusLink.classList.add("active"); //adds active class in HTML
@@ -68,14 +60,14 @@ function changeDisplay(response) {
   let currentTemp = Math.round(response.data.main.temp);
   let humidity = response.data.main.humidity;
   let wind = Math.round(response.data.wind.speed);
+  //global variables
+  celsiusTemp = Math.round(response.data.main.temp); //redefines global variable of celsiusTemp
+  cityWind = Math.round(response.data.wind.speed); //redefines global variable of cityWind
   //query selectors
   let cityHeader = document.querySelector("#the-city");
   let theTemp = document.querySelector("#the-temperature");
   let theHumidity = document.querySelector("#the-humidity");
   let theWindSpeed = document.querySelector("#wind-speed");
-  //global variables
-  celsiusTemp = Math.round(response.data.main.temp); //redefines global variable of celsiusTemp
-  cityWind = Math.round(response.data.wind.speed); //redefines global variable of cityWind
   //innerHTML changes
   cityHeader.innerHTML = `${cityName}`; //changes display of city
   theTemp.innerHTML = `${currentTemp}`; //changes display of temperature
@@ -83,15 +75,31 @@ function changeDisplay(response) {
   theWindSpeed.innerHTML = `${wind}`; //changes display of wind speed
   //call to displayDate function
   displayDate();
+  //call to retrieveHourly function
+  retrieveHourly(response.data.name);
 }
-//called from "main" function or by addEventListener
-//obtains position of user
-//calls getCoordinates function
+/*retrieveHourly function
+-- receives a parameter
+-- connects to weather API 
+-- calls displayHourly function*/
+function retrieveHourly(city) {
+  let apiKey = `f655251e7aa74c3031f8eb126912bec6`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayHourly);
+}
+function displayHourly(response) {
+  console.log(response.data);
+}
+/*getPosition function
+-- called from "main" function or by addEventListener
+-- obtains position of user
+-- calls getCoordinates function*/
 function getPosition() {
   navigator.geolocation.getCurrentPosition(getCoordinates);
 }
-//called by getPosition function
-//obtains latitude and longitude of user
+/*getCoordinates function
+-- called by getPosition function
+-- obtains latitude and longitude of user*/
 function getCoordinates(position) {
   //local variables
   let lat = position.coords.latitude;
@@ -99,20 +107,22 @@ function getCoordinates(position) {
   //call to getTemp function
   getTemp(lat, lon);
 }
-//called by getCoordinated function
-//retrieves two parameters
-//finds user location with latitude and longitude
-//calls changeDisplay function
+/*getTemp function
+-- called by getCoordinated function
+-- retrieves two parameters
+-- finds user location with latitude and longitude
+-- calls changeDisplay function*/
 function getTemp(lat, lon) {
   //local variables
   let apiKey = `f655251e7aa74c3031f8eb126912bec6`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  //link to weather API
+  //connects to weather API
   axios.get(apiUrl).then(changeDisplay);
 }
-//called by addEventListener
-//converts units from metric to imperial
-//changes display of info
+/*displayImperial function
+-- called by addEventListener
+-- converts units from metric to imperial
+-- changes display of info*/
 function displayImperial(event) {
   event.preventDefault();
 
@@ -126,12 +136,13 @@ function displayImperial(event) {
   let unit = document.querySelector("#wind-unit");
   //innerHTML chagnes
   temperature.innerHTML = Math.round(fTemp); //changes display of temperature
-  wind.innerHTML = Math.round(cityWind / 1.609);
-  unit.innerHTML = `mph`;
+  wind.innerHTML = Math.round(cityWind / 1.609); //changes display of wind speed
+  unit.innerHTML = `mph`; //changes display of wind speed units
 }
-//called by addEventListener
-//converts units from imperial to metric
-//changes display of info
+/*displayMetric function
+-- called by addEventListener
+-- converts units from imperial to metric
+-- changes display of info*/
 function displayMetric(event) {
   event.preventDefault();
 
@@ -143,26 +154,25 @@ function displayMetric(event) {
   let unit = document.querySelector("#wind-unit");
   //innerHTML changes
   temperature.innerHTML = celsiusTemp; //changes display of temperature
-  wind.innerHTML = cityWind;
-  unit.innerHTML = `km/h`;
+  wind.innerHTML = cityWind; //changes display of wind speed
+  unit.innerHTML = `km/h`; //changes display of wind speed units
 }
 //***END: defined functions***
 
-//***START: calls to functions***
-
+//global variables
 let cityWind = null;
 let celsiusTemp = null;
-
+//query selectors
 let currentLocation = document.querySelector("#location"); //initializes currentLocation to the button in HTML
 let fahrenheitLink = document.querySelector("#fahrenheit-link"); //initializes fahrenheitLink to the link in HTML
 let celsiusLink = document.querySelector("#celsius-link"); //initializes celsiusLink to the link in HTML
-
+//addEventListeners
 currentLocation.addEventListener("click", getPosition); //calls function getPosition if user clicks current location button
-fahrenheitLink.addEventListener("click", displayImperial);
-celsiusLink.addEventListener("click", displayMetric);
-//calls getPosition function
+fahrenheitLink.addEventListener("click", displayImperial); //calls displayImperial function if user clicks fahrenheit
+celsiusLink.addEventListener("click", displayMetric); //calls displayMetric function if user clicks celsius
+//call to getPosition function
 getPosition();
-//calls displayDate function
+//call to displayDate function
 displayDate();
-//calls searchCity function
+//call to searchCity function
 submitCity();
